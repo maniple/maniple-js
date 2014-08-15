f5a5004c017484fb04e01a9719b1c2698335a0b8
xemlock <xemlock@gmail.com>
2014-08-15 09:37:16 +0200

    distdir resolved relatively to .distdir file

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,6 @@
 module.exports = function(grunt) {
 
+    var path = require('path');
     var findup = require('findup-sync');
 
     var distdirPath = findup('maniple-js.distdir');
@@ -9,14 +10,19 @@ module.exports = function(grunt) {
     var DISTDIR;
 
     if (distdirPath) {
+        // read path to DISTDIR from .distdir file
+        // if path is relative, resolve it relatively to .distdir file
         DISTDIR = grunt.file.read(distdirPath).trim();
+        if (!grunt.file.isPathAbsolute(DISTDIR)) {
+            DISTDIR = path.dirname(distdirPath) + path.sep + DISTDIR;
+        }
     } else {
         DISTDIR ='dist/maniple';
     }
 
     grunt.file.recurse('src', function (abspath, rootdir, subdir, filename) {
         if (filename.match(/\.js$/)) {
-            files[DISTDIR + (subdir ? '/' + subdir : '') + '/' + filename] = abspath;
+            files[DISTDIR + (subdir ? path.sep + subdir : '') + path.sep + filename] = abspath;
         }
     });
 
